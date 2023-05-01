d47ecfda9c566084e6bdb02566af17d895f31003
karim-abdul-1 <karimabdul5519@gmail.com>
2023-05-01 14:40:11 +0600

    5 value for loop complete

---
+++ For-loop/5_value_and printf-that.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+
+{
+    int i,n,a;
+    printf("Enter a number : ");
+    scanf("%d %d %d %d %d",&n);
+    for(i=1; i<=5; i=i+1)
+    {
+        printf("Enter number : %d",a);
+
+
+
+    }
+    printf("Number is = %d\n",a);
+    return 0;
+}
